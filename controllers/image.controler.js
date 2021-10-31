842e7a32f249df91a88979a71a4b228abb2fe774
imdeepchand <imdeepchanddewangan@gmail.com>
2021-10-31 19:16:37 +0530

    update

--- controllers/image.controler.js
+++ controllers/image.controler.js
@@ -1,7 +1,6 @@
 const { config } = require("../config/config");
 const imageSchema = require("../models/image.model");
 const { ObjectId } = require("mongodb");
-const unlinkImage = require("../midelware/fs.midelware");
 exports.store = (req, res, next) => {
   imageSchema.create(
     {
@@ -31,10 +30,6 @@ exports.getImage = (req, res, next) => {
 
 exports.updateById = (req, res) => {
   const where = { _id: new ObjectId(req.params.id) };
-  imageSchema.findOne(where, (error, data) => {
-    if (error) console.log(error);
-    else {
-      unlinkImage(data.image);
       imageSchema.findByIdAndUpdate(
         where,
         {
@@ -50,23 +45,15 @@ exports.updateById = (req, res) => {
           }
         }
       );
-    }
-  });
 };
 
 exports.deleteById = (req, res) => {
   const where = { _id: new ObjectId(req.params.id) };
-  imageSchema.findOne(where, (error, data) => {
-    if (error) console.log(error);
-    else {
-      unlinkImage(data.image);
-      imageSchema.findByIdAndRemove(where, (error, data) => {
-        if (error) {
-          return next(error);
-        } else {
-          res.status(301).json({ msg: "successfully deleted!" });
-        }
-      });
+  imageSchema.findByIdAndRemove(where, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(301).json({ msg: "successfully deleted!" });
     }
   });
 };
--- routes/image.routes.js
+++ routes/image.routes.js
@@ -1,14 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const auth = require('../helpers/jwt.helper');
-const { store, getImage, updateById, deleteById } = require('../controllers/image.controler');
-const { uploadImg } = require('../midelware/upload.midel');
+const auth = require("../helpers/jwt.helper");
+const {
+  store,
+  getImage,
+  updateById,
+  deleteById,
+} = require("../controllers/image.controler");
+const { uploadImg } = require("../midelware/upload.midel");
+const unlinkImage = require("../midelware/fs.midelware");
 // Express route
 const imageRouter = express.Router();
 
-//image 
-imageRouter.post('/image', auth,uploadImg, store);
-imageRouter.post('/update/:id',auth,uploadImg,updateById);
-imageRouter.post('/delete/:id',auth,deleteById);
-imageRouter.get('/getimage', auth,getImage);
+//image
+imageRouter.post("/image", auth, uploadImg, store);
+imageRouter.post("/update/:id", auth, unlinkImage, uploadImg, updateById);
+imageRouter.post("/delete/:id", auth, unlinkImage, deleteById);
+imageRouter.get("/getimage", auth, getImage);
 module.exports = imageRouter;
