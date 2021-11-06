6c8a57208128bc68a99cacb5830f84795576979e
imdeepchand <imdeepchanddewangan@gmail.com>
2021-11-06 16:06:27 +0530

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,25 +1,28 @@
 const Bcrypt = require("bcryptjs");
-const userSchema = require('../models/users.model');
-const jwt = require('jsonwebtoken');
-const { RFC, LOGIN_SUCCESS, INVALID_USER } = require('../frozen/msgAndStatusCode');
+const userSchema = require("../models/users.model");
+const jwt = require("jsonwebtoken");
+const {
+  RFC,
+  LOGIN_SUCCESS,
+  INVALID_USER,
+} = require("../frozen/msgAndStatusCode");
 
-exports.Login = async (req,res) => {
-    const where = {email: req.body.email};
-    const user = await userSchema.find(where);
-    const data = {}
-    jwt.sign({user}, 'secretkey', { expiresIn: '2h' }, (err, token) => {
-        data.user = user
-        data.token = token 
-      });
-    if(user.length === 0){
-        res.status(RFC.H401).json({msg: INVALID_USER});
-    }
-    else {
+exports.Login = async (req, res) => {
+  const where = { email: req.body.email };
+  const user = await userSchema.find(where);
+  const data = {};
+  jwt.sign({ user }, "secretkey", { expiresIn: "2h" }, (err, token) => {
+    data.user = user;
+    data.token = token;
+  });
+  if (user.length === 0) {
+    res.status(RFC.H401).json({ msg: INVALID_USER });
+  } else {
     const cmp = await Bcrypt.compare(req.body.password, user[0].password);
-        if(cmp){
-            res.status(RFC.H200).json({data:data, msg: LOGIN_SUCCESS })
-        } else {
-            res.status(RFC.H401).json({msg: INVALID_USER});
-        }
+    if (cmp) {
+      res.status(RFC.H200).json({ data: data, msg: LOGIN_SUCCESS });
+    } else {
+      res.status(RFC.H401).json({ msg: INVALID_USER });
     }
-}
+  }
+};
--- helpers/jwt.helper.js
+++ helpers/jwt.helper.js
@@ -1,18 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const verifyToken = (req,res, next) => {
+const verifyToken = (req, res, next) => {
   const token = req.body.token || req.query.token || req.headers.token;
 
-  if(!token) {
+  if (!token) {
     return res.status(403).send("A token is requires for authentication");
   }
-  try{
-    const decoded = jwt.verify(token,'secretkey');
+  try {
+    const decoded = jwt.verify(token, "secretkey");
     req.user = decoded;
   } catch (err) {
     return res.status(401).send("Invalid Token");
   }
   return next();
-}
+};
 
 module.exports = verifyToken;
